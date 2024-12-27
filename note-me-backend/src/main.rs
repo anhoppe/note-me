03af014b9fbdf426f384eeec1a1609de3e9f6ecc
anhoppe <anhoppe@gmail.com>
2024-12-27 15:02:16 +0100

    1739593500 set backend port to 8080

--- note-me-backend/src/main.rs
+++ note-me-backend/src/main.rs
@@ -7,5 +7,5 @@ use crate::server::Server;
 async fn main() {
     let server = Server::new();
     let ip = String::from("127.0.0.1");
-    server.serve(&ip, 3000).await;
+    server.serve(&ip, 8080).await;
 }

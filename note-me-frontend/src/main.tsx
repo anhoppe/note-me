d34a9df455db67bf98d837f4d116f5e25e50b4fa
anhoppe <anhoppe@gmail.com>
2025-01-05 14:49:17 +0100

    1755904884 added login page and configured Auth0Provider

--- note-me-frontend/src/main.tsx
+++ note-me-frontend/src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { Auth0Provider } from '@auth0/auth0-react';
+import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
+
 import './index.css'
 import App from './App.tsx'
+import LoginButton from './login';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Auth0Provider
+      domain="dev-40fxc54pknc85fku.us.auth0.com"
+      clientId="xmcJLDXZCnFO6pS7s9mbJnm0rNbNLVqx"
+      authorizationParams={{
+        redirect_uri: "https://note-me-git-main-anhoppes-projects.vercel.app/"
+        // redirect_uri: window.location.origin
+      }}
+    >
+      <LoginButton />
+      <App />
+    </Auth0Provider>
+  </StrictMode>
 )
